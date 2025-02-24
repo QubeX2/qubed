e5cc94fa96ec20b903166385b104bc7f358e827d
Mikael Andersson <qubex2@gmail.com>
2025-02-24 23:46:13 +0100

    w

--- qubed.c
+++ qubed.c
@@ -1,34 +1,249 @@
 #include "qubed.h"
-#include <ctype.h>
+#include <asm-generic/ioctls.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
-struct termios orig_termios;
+struct editor_config E;
 
 int main(void) {
   enable_raw_mode();
+  init_editor();
 
-  char c;
-  while (read(STDIN_FILENO, &c, 1 == 1 && c != 'q')) {
-    if (iscntrl(c)) {
-      printf("%d\n", c);
-    } else {
-      printf("%d ('%c')\n", c, c);
-    }
+  while (1) {
+    editor_refresh_screen();
+    editor_process_keypress();
   }
 
   return 0;
 }
 
-void enable_raw_mode() {
-  tcgetattr(STDIN_FILENO, &orig_termios);
-  atexit(disable_raw_mode);
+/**
+ *
+ */
+void init_editor() {
+  E.cx = 0;
+  E.cy = 0;
 
-  struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON);
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  if (get_window_size(&E.screen_rows, &E.screen_cols) == -1)
+    die("get_window_size");
 }
 
-void disable_raw_mode() { tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); }
+/**
+ *
+ */
+void editor_move_cursor(char key) {
+  switch (key) {
+  case 'h':
+    E.cx--;
+    break;
+  case 'l':
+    E.cx++;
+    break;
+  case 'k':
+    E.cy--;
+    break;
+  case 'j':
+    E.cy++;
+    break;
+  }
+}
+
+/**
+ *
+ */
+int get_cursor_position(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+    return -1;
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1)
+      break;
+    if (buf[i] == 'R')
+      break;
+    i++;
+  }
+  buf[i] = '\0';
+  if (buf[0] != '\x1b' || buf[1] != '[')
+    return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+    return -1;
+  return 0;
+}
+
+/**
+ *
+ */
+int get_window_size(int *rows, int *cols) {
+  struct winsize ws;
+
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+      return -1;
+
+    return get_cursor_position(rows, cols);
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
+/**
+ *
+ */
+void editor_draw_rows(struct abuf *ab) {
+  for (int y = 0; y < E.screen_rows; y++) {
+    if (y == E.screen_rows / 3) {
+      char welcome[80];
+      int welclen = snprintf(welcome, sizeof(welcome),
+                             "qubed editor -- version %s", QUBED_VERSION);
+      if (welclen > E.screen_cols)
+        welclen = E.screen_cols;
+
+      int padding = (E.screen_cols - welclen) / 2;
+      if (padding) {
+        ab_append(ab, "~", 1);
+        padding--;
+      }
+      while (padding--)
+        ab_append(ab, " ", 1);
+
+      ab_append(ab, welcome, welclen);
+    } else {
+      ab_append(ab, "~", 1);
+    }
+
+    ab_append(ab, "\x1b[K", 3);
+    if (y < E.screen_rows - 1) {
+      ab_append(ab, "\r\n", 2);
+    }
+  }
+}
+
+/**
+ *
+ */
+void editor_clear_screen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+/**
+ *
+ */
+void editor_refresh_screen() {
+  struct abuf ab = ABUF_INIT;
+
+  ab_append(&ab, "\x1b[?25l", 6);
+  ab_append(&ab, "\x1b[H", 3);
+
+  editor_draw_rows(&ab);
+
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  ab_append(&ab, buf, strlen(buf));
+
+  // ab_append(&ab, "\x1b[H", 3);
+  ab_append(&ab, "\x1b[?25h", 6);
+
+  write(STDOUT_FILENO, ab.b, ab.len);
+  ab_free(&ab);
+}
+
+/**
+ *,
+ */
+char editor_read_key() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN)
+      die("read");
+  }
+  return c;
+}
+
+/**
+ *
+ */
+void editor_process_keypress() {
+  char c = editor_read_key();
+
+  switch (c) {
+  case CTRL_KEY('q'):
+    editor_clear_screen();
+    exit(0);
+    break;
+
+  case 'h':
+  case 'j':
+  case 'k':
+  case 'l':
+    editor_move_cursor(c);
+    break;
+  }
+}
+
+/**
+ * enable_raw_mode
+ */
+void enable_raw_mode() {
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
+    die("tcgetattr");
+
+  atexit(disable_raw_mode);
+
+  struct termios raw = E.orig_termios;
+  raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP | ICRNL | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ICRNL | ECHO | IEXTEN | ICANON | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+    die("tcsetattr");
+}
+
+/**
+ * disable_raw_mode
+ */
+void disable_raw_mode() {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
+    die("tcsetattr");
+}
+
+/**
+ *
+ */
+void ab_append(struct abuf *ab, const char *s, int len) {
+  char *new = realloc(ab->b, ab->len + len);
+
+  if (new == NULL)
+    return;
+
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab->len += len;
+}
+
+/**
+ *
+ */
+void ab_free(struct abuf *ab) { free(ab->b); }
+
+/**
+ * die
+ */
+void die(const char *s) {
+  editor_clear_screen();
+
+  perror(s);
+  exit(1);
+}
--- qubed.h
+++ qubed.h
@@ -1,7 +1,37 @@
 #ifndef QUBED_H
 #define QUBED_H
 
+#include <termios.h>
+
+#define QUBED_VERSION "0.0.1"
+#define CTRL_KEY(k) ((k) & 0x1f)
+#define ABUF_INIT {NULL, 0}
+
+struct editor_config {
+  int cx, cy;
+  int screen_rows, screen_cols;
+  struct termios orig_termios;
+};
+
+struct abuf {
+  char *b;
+  int len;
+};
+
 void enable_raw_mode();
 void disable_raw_mode();
+void die(const char *s);
+void editor_process_keypress();
+char editor_read_key();
+void editor_refresh_screen();
+void editor_clear_screen();
+void editor_draw_rows(struct abuf *ab);
+void editor_move_cursor(char key);
+int get_window_size(int *rows, int *cols);
+int get_cursor_position(int *rows, int *cols);
+void ab_append(struct abuf *ab, const char *s, int len);
+void ab_free(struct abuf *ab);
+
+void init_editor();
 
 #endif
